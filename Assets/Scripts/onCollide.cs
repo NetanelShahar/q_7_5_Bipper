bbf6967edb8b28bcddff9398ca9b131aed9e1d9e
netanelbenis <netnel.b.i@gmail.com>
2020-05-12 14:14:23 +0300

    sec

---
+++ Assets/Scripts/onCollide.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class onCollide : MonoBehaviour
+{
+    public AudioSource[] sounds;
+    public AudioSource noise1;
+    private void Start() {
+    
+    sounds = GetComponents<AudioSource>();
+        noise1 = sounds[0];
+    }
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log(this.name + " Trigger with name=" + other.name+" tag="+other.tag);
+        noise1.Play();
+
+    }
+}
